db5990eab1a04edaa62d440730952b2b232cfc87
argianW <argian.wb@gmail.com>
2022-09-23 15:14:59 +0700

    fix

--- src/main/kotlin/09.kt
+++ src/main/kotlin/09.kt
@@ -20,10 +20,10 @@ fun main() {
     var peoole: ArrayList<People> = ArrayList()
     peoole.add(People(1,"Udin", 12))
     peoole.add(People(2, "Wati", 51))
-    peoole.add(People(2, "Budi", 34))
-    peoole.add(People(2, "Agus", 16))
-    peoole.add(People(2, "Sari", 19))
-    peoole.add(People(2, "Riri", 21))
+    peoole.add(People(3, "Budi", 34))
+    peoole.add(People(4, "Agus", 16))
+    peoole.add(People(5, "Sari", 19))
+    peoole.add(People(6, "Riri", 21))
 
     var filterPeople = peoole.filter { it.age <= 21}
     println(filterPeople)
